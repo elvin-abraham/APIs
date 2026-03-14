c568292a4a4b019e40819b83c7f1837c16b8563a
Elvin Abraham <elvinmabraham@gmail.com>
2026-03-14 23:22:43 +0530

    simple api creation practice

--- server.js
+++ server.js
@@ -2,23 +2,13 @@ import express from 'express';
 
 const app=express();
 
-const users=[
-    {id:1,name:'Arun'},
-    {id:2,name:'Anoop'}
-]
-
-app.get('/users/:id',(req,res)=>{
-    let id=Number(req.params.id);
-
-    let user=users.find(u=> u.id === id)
-    if(!user){
-        res.json({
-            message:"user not exists"
-        })
-    }
-    res.json(user);
+app.get('/message',(req,res)=>{
+    res.json({
+        message:'Hello welcome'
+    })
 })
 
+
 app.listen(3000,()=>{
-    console.log('app is running');
+    console.log('app is running')
 })
