c77ab2ac1f2d1ae090165bcd962be06a73593d6c
Elvin Abraham <elvinmabraham@gmail.com>
2026-03-15 22:43:07 +0530

    simple api practice 2

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
 import express from 'express';
 
 const app=express();
-app.use(express.json());
 
-app.post('/multiply',(req,res)=>{
-   let num1=Number(req.body.num1);
-   let num2=Number(req.body.num2);
-   let result=num1*num2;
-   res.json({
-    result:result
-   })
+app.get('/testapi',(req,res)=>{
+    res.json({
+        message:"test api message"
+    })
 })
 
-
 app.listen(3000,()=>{
-    console.log('app is running')
+    console.log('app is running');
 })
