3cfb74116b9f5e28d32d9efedd5a50a5ecab4b24
Elvin Abraham <elvinmabraham@gmail.com>
2026-03-17 23:44:19 +0530

    api to find the specific user practice 2

--- server.js
+++ server.js
@@ -7,8 +7,18 @@ const app=express();
         {id:2,name:"Jithin"}
     ]
 
-app.get('/users',(req,res)=>{
-    res.json(users);
+app.get('/user/:id',(req,res)=>{
+    let id=Number(req.params.id);
+    let user=users.find((u)=>{
+        return u.id===id;
+    })
+    if(user){
+        res.json(user)
+    }else{
+        res.json({
+            message:"No user found"
+        })
+    }
 })
 
 app.listen(3000,()=>{
