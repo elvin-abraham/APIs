0e2e921df0c4df4c00f5b43b346f685980fd6d2b
Elvin Abraham <elvinmabraham@gmail.com>
2026-03-18 21:11:39 +0530

    API to create a student in mongodb

--- server.js
+++ server.js
@@ -1,24 +1,44 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app=express();
+app.use(express.json());
 
-   const users=[
-        {id:1,name:"Elvin"},
-        {id:2,name:"Jithin"}
-    ]
+mongoose.connect("mongodb://127.0.0.1:27017/schoolDB")
+.then(()=>{
+    console.log("Mongodb connected")
+}).catch((err)=>{
+    console.log(err)
+})
 
-app.get('/user/:id',(req,res)=>{
-    let id=Number(req.params.id);
-    let user=users.find((u)=>{
-        return u.id===id;
+const studentSchema=new mongoose.Schema({
+    name:String,
+    age:Number,
+    course:String
+})
+
+const Student=mongoose.model("Student",studentSchema)
+
+app.post('/students',async (req,res)=>{
+  try {
+       const {name,age,course}=req.body;
+     const newStudent=new Student({
+        name,
+        age,
+        course
+     })
+
+     await newStudent.save();
+     res.json({
+        message:"Student Added",
+        Details:newStudent
+     })
+  } catch (error) {
+    res.status(500).json({
+        message:"Error while adding student",
+        error:error.message
     })
-    if(user){
-        res.json(user)
-    }else{
-        res.json({
-            message:"No user found"
-        })
-    }
+  }
 })
 
 app.listen(3000,()=>{
